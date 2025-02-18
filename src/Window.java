ddc6b7f835c8d9c3df329775f8a4e40b93bc18da
CosmoUrMom <rasheedabbasov@gmail.com>
2025-02-18 10:57:15 -0500

    ADDED ROUND CORNERS TO BUTTONS

--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,12 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import javax.swing.*;
+import javax.swing.border.*;
 
 
 public class Window extends JFrame{
@@ -63,6 +68,10 @@ public class Window extends JFrame{
         //Sets the size of the button
         soundButton1.setPreferredSize(new Dimension(200,50));
         Button2.setPreferredSize(new Dimension(200,50));
+
+        makeButtonRound(soundButton1);
+        makeButtonRound(Button2);
+        makeButtonRound(soundButton2);
         
         
         //Sets the size of the panels 
@@ -72,10 +81,11 @@ public class Window extends JFrame{
         panelwest.setPreferredSize(new Dimension(300, 100));
         panelsouth.setPreferredSize(new Dimension(300 ,100));
 
+        //Sets a new grid layout for center panel
         panelcenter.setLayout(new GridLayout(2,2,2,10));
         
 
-        //Adds elements to the sub pnales
+        //Adds elements to the sub panels
         panelcenter.add(this.soundButton1);
         panelcenter.add(this.soundButton2);
         panelnorth.add(this.Button2);
@@ -86,6 +96,45 @@ public class Window extends JFrame{
         contentPane.add(panelcenter, BorderLayout.CENTER);
     }
 
+    private JButton makeButton(String name) {
+
+        return new JButton("name");
+
+    }
+
+    private void makeButtonRound(JButton button) {
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false);
+        button.setBorder(new RoundedBorder(30)); //Set rounded corners with 30 px radius
+        button.setBackground(Color.CYAN); //Optional set background color
+    }
+
+    class RoundedBorder implements Border {
+        
+        private int radius;
+
+
+        public RoundedBorder (int radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius + 5, this.radius + 5, this.radius + 5, this.radius + 5);
+        }
+
+        @Override
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.setColor(Color.BLACK); //Background Color
+            g.fillRoundRect(x, y, width -1, height - 1, radius, radius);
+        }
+        
+    }
 
 
     //Sets the user set a custom name for a button
