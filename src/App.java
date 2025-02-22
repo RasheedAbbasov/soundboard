5ab290b9ff3cc6edc8c75e56611ac39e5f8a7b3e
CosmoUrMom <rasheedabbasov@gmail.com>
2025-02-22 17:46:51 -0500

    CHANGED LAYOUT

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scnr = new Scanner(System.in);
 
 
 
--- src/Window.java
+++ src/Window.java
@@ -1,19 +1,22 @@
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
 
 public class Window extends JFrame{
 
-
+    private Scanner scnr;
+    private static String filename = "button.txt";
     private String soundName;
     private JPanel contentPane = new JPanel();
     private ImageIcon Icon = new ImageIcon("soundboard.png");
     private JLabel label = new JLabel("Test");
-    private JButton soundButton1 = new JButton("1");
-    private JButton soundButton2 = new JButton("2");
-    private JButton Button2 = new JButton("Place Holder");
+    private JButton soundButton1 = new JButton("sound1");
+    private JButton soundButton2 = new JButton("sound2");
+    private JButton importButton = new JButton("Import");
+    private JButton deleteButton = new JButton("Delete");
+    private JButton editButton = new JButton("Edit");
 
 
 
@@ -24,15 +27,15 @@ public class Window extends JFrame{
 
         //Sets the properties 
         this.setTitle("Rasho's SoundBoard");
-        this.setSize(1920,1080);
+        this.setSize(500,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setIconImage(Icon.getImage());
 
         //Sets the frame to full screen
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         //Hides the title when set to full screen
-        this.setUndecorated(true);
+        // this.setUndecorated(true);
 
         //Set the custom layout
         customLayout();
@@ -50,45 +53,50 @@ public class Window extends JFrame{
      * Sets the layout of the soundboard
      */
     private void customLayout() {
-        this.contentPane.setLayout(new BorderLayout());
+        this.contentPane.setLayout(new FlowLayout());
 
         //Creates Center and North sub panels
         JPanel panelcenter = new JPanel();
         JPanel panelnorth = new JPanel();
-        JPanel paneleast = new JPanel();
-        JPanel panelwest = new JPanel();
-        JPanel panelsouth = new JPanel();
 
 
-        //Sets the size of the button
+        //Sets the size of the buttons
         soundButton1.setPreferredSize(new Dimension(200,50));
-        Button2.setPreferredSize(new Dimension(200,50));
+        soundButton2.setPreferredSize(new Dimension(200, 50));
+        importButton.setPreferredSize(new Dimension(100,50));
+        deleteButton.setPreferredSize(new Dimension(100,50));
+        editButton.setPreferredSize(new Dimension(100, 50));
 
-        makeButtonRound(soundButton1);
-        makeButtonRound(Button2);
-        makeButtonRound(soundButton2);
+
+        // makeButtonRound(soundButton1);
+        // makeButtonRound(Button2);
+        // makeButtonRound(soundButton2);
         
         
-        //Sets the size of the panels 
-        panelcenter.setPreferredSize(new Dimension(300,100));
-        panelnorth.setPreferredSize(new Dimension(300, 100));
-        paneleast.setPreferredSize(new Dimension(300, 100));
-        panelwest.setPreferredSize(new Dimension(300, 100));
-        panelsouth.setPreferredSize(new Dimension(300 ,100));
+        //Sets the size and the color of the panels
+        panelcenter.setPreferredSize(new Dimension(500,500));
+        panelcenter.setBackground(Color.LIGHT_GRAY);
+        panelnorth.setPreferredSize(new Dimension(500, 100));
+        panelnorth.setBackground(Color.BLUE);
 
-        //Sets a new grid layout for center panel
-        panelcenter.setLayout(new GridLayout(2,2,2,10));
+        //Sets a new grid layout for sub panels
+        panelcenter.setLayout(new FlowLayout());
+        panelnorth.setLayout(new FlowLayout());
         
 
         //Adds elements to the sub panels
-        panelcenter.add(this.soundButton1);
-        panelcenter.add(this.soundButton2);
-        panelnorth.add(this.Button2);
+        panelcenter.add(soundButton1);
+        panelcenter.add(soundButton2);
+        panelnorth.add(importButton);
+        panelnorth.add(deleteButton);
+        panelnorth.add(editButton);
+
+;
 
         
         //adds the subpanels to the main panel
         contentPane.add(panelnorth, BorderLayout.NORTH);
-        contentPane.add(panelcenter, BorderLayout.CENTER);
+        contentPane.add(panelcenter, FlowLayout.CENTER);
     }
 
     private JButton makeButton(String name) {
