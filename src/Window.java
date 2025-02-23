0ab443783c424168ff1e59e4784d35b4cd0139d4
CosmoUrMom <rasheedabbasov@gmail.com>
2025-02-23 11:20:41 -0500

    IMPLEMENTED METHOD TO ADD BUTTONS

--- src/Window.java
+++ src/Window.java
@@ -1,31 +1,28 @@
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
-import javax.swing.border.*;
 
-
-public class Window extends JFrame{
+public class Window extends JFrame {
 
     private Scanner scnr;
     private static String filename = "button.txt";
     private String soundName;
     private JPanel contentPane = new JPanel();
+    private JPanel panelCenter = new JPanel();
     private ImageIcon Icon = new ImageIcon("soundboard.png");
-    private JLabel label = new JLabel("Test");
-    private JButton soundButton1 = new JButton("sound1");
-    private JButton soundButton2 = new JButton("sound2");
     private final JButton importButton = new JButton("Import");
     private final JButton deleteButton = new JButton("Delete");
     private final JButton editButton = new JButton("Edit");
     private SoundManager soundManager;
 
-
-
-
     /**
      * Constructor
      */
     public Window() {
+
+
+        //Initializing sound manager
+        soundManager = new SoundManager();
 
         //Sets the properties 
         this.setTitle("Rasho's SoundBoard");
@@ -42,6 +39,10 @@ public class Window extends JFrame{
         //Set the custom layout
         customLayout();
 
+        //Loading the saved sounds
+        loadSavedSounds();
+        
+
         //Set the contentPane to the frame
         this.setContentPane(contentPane);
 
@@ -49,114 +50,116 @@ public class Window extends JFrame{
         this.setVisible(true);
     }
 
-
     /**
      * Sets the layout of the soundboard
      */
     private void customLayout() {
         this.contentPane.setLayout(new FlowLayout());
 
-        //Creates Center and North sub panels
-        JPanel panelcenter = new JPanel();
+        // Creates Center and North sub panels
         JPanel panelnorth = new JPanel();
 
-
-
-
-
-        //Sets the size of the buttons
-        soundButton1.setPreferredSize(new Dimension(150,50));
-        soundButton2.setPreferredSize(new Dimension(150, 50));
-        importButton.setPreferredSize(new Dimension(100,50));
-        deleteButton.setPreferredSize(new Dimension(100,50));
+        // Sets the size of the buttons
+        importButton.setPreferredSize(new Dimension(100, 50));
+        deleteButton.setPreferredSize(new Dimension(100, 50));
         editButton.setPreferredSize(new Dimension(100, 50));
-
-
-
 
         // makeButtonRound(soundButton1);
         // makeButtonRound(Button2);
         // makeButtonRound(soundButton2);
-        
-        
-        //Sets the size and the color of the panels
-        panelcenter.setPreferredSize(new Dimension(500,500));
-        panelcenter.setBackground(Color.LIGHT_GRAY);
+
+        // Sets the size and the color of the panels
+        panelCenter.setPreferredSize(new Dimension(500, 500));
+        panelCenter.setBackground(Color.LIGHT_GRAY);
         panelnorth.setPreferredSize(new Dimension(500, 100));
         panelnorth.setBackground(Color.BLUE);
 
-        //Sets a new grid layout for sub panels
-        panelcenter.setLayout(new FlowLayout());
+        // Sets a new grid layout for sub panels
+        panelCenter.setLayout(new FlowLayout());
         panelnorth.setLayout(new FlowLayout());
-        
 
-        //Adds elements to the sub panels
-        panelcenter.add(soundButton1);
-        soundButton1 = new JButton("hey");
-        panelcenter.add(soundButton1);
-        panelcenter.add(soundButton2);
+        // Adds elements to the sub panels
         panelnorth.add(importButton);
         panelnorth.add(deleteButton);
         panelnorth.add(editButton);
 
+        ;
 
-;
-
-        
-        //adds the subpanels to the main panel
+        // adds the subpanels to the main panel
         contentPane.add(panelnorth, BorderLayout.NORTH);
-        contentPane.add(panelcenter, FlowLayout.CENTER);
+        contentPane.add(panelCenter, FlowLayout.CENTER);
     }
 
-    private JButton makeButton(String name) {
-
-        return new JButton("name");
-
+    /**
+     * Loads the saved sounds from properties and adds them as buttons
+     */
+    private void loadSavedSounds() {
+        Map<String, String> sounds = soundManager.getAllSounds();
+        for (String soundName : sounds.keySet()) {
+            addSoundButton(soundName, sounds.get(soundName));
+        }
     }
 
-    private ArrayList<String> readData() {
-        ArrayList<String> buttonName = new ArrayList<>();
-        return buttonName;
+    /**
+     * Takes in the name and the location of the sound and stores it in a hashmap
+     * and adds it to the center panel
+     * 
+     * 
+     * @param soundName the name of the sounds that is going to be added
+     * @param filePath  the location of the sounds
+     */
+    private void addSoundButton(String soundName, String filePath) {
+        JButton soundButton = new JButton(soundName);
+        panelCenter.add(soundButton);
+        panelCenter.revalidate();
+        panelCenter.repaint();
     }
 
-    private void makeButtonRound(JButton button) {
-        button.setFocusPainted(false);
-        button.setContentAreaFilled(false);
-        button.setBorder(new RoundedBorder(30)); //Set rounded corners with 30 px radius
+    private void addNewSound() {
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(this);
         
     }
 
-    class RoundedBorder implements Border {
-        
-        private int radius;
+    // private void makeButtonRound(JButton button) {
+    // button.setFocusPainted(false);
+    // button.setContentAreaFilled(false);
+    // button.setBorder(new RoundedBorder(30)); //Set rounded corners with 30 px
+    // radius
 
+    // }
 
-        public RoundedBorder (int radius) {
-            this.radius = radius;
-        }
+    // class RoundedBorder implements Border {
 
-        @Override
-        public Insets getBorderInsets(Component c) {
-            return new Insets(this.radius + 5, this.radius + 5, this.radius + 5, this.radius + 5);
-        }
+    // private int radius;
 
-        @Override
-        public boolean isBorderOpaque() {
-            return true;
-        }
+    // public RoundedBorder (int radius) {
+    // this.radius = radius;
+    // }
 
-        @Override
-        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-             //Background Color
-            g.fillRoundRect(x, y, width -1, height - 1, radius, radius);
-        }
-        
-    }
+    // @Override
+    // public Insets getBorderInsets(Component c) {
+    // return new Insets(this.radius + 5, this.radius + 5, this.radius + 5,
+    // this.radius + 5);
+    // }
 
+    // @Override
+    // public boolean isBorderOpaque() {
+    // return true;
+    // }
 
-    //Sets the user set a custom name for a button
-    public void setSoundName(String name) {
-        this.soundName = name;
-    }
+    // @Override
+    // public void paintBorder(Component c, Graphics g, int x, int y, int width, int
+    // height) {
+    // //Background Color
+    // g.fillRoundRect(x, y, width -1, height - 1, radius, radius);
+    // }
+
+    // }
+
+    // //Sets the user set a custom name for a button
+    // public void setSoundName(String name) {
+    // this.soundName = name;
+    // }
 
 }
